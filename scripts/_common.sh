b4fe63f9a756add8f75b0addd1ec2ee36ab1ff27
tituspijean <tituspijean@outlook.com>
2020-03-20 21:16:35 +0100

    [upg] nodejs to v12

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version=10
+nodejs_version=12
 
 #=================================================
 # PERSONAL HELPERS
