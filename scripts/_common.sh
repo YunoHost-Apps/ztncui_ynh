02bb04a50cc3edeb9852d742296f8c89f62606da
tituspijean <tituspijean@outlook.com>
2020-04-19 16:11:52 +0200

    [fix] ...

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -97,8 +97,6 @@ ynh_add_systemd_config_vars () {
 # FUTURE OFFICIAL HELPERS
 #=================================================
 
-#!/bin/bash
-
 n_install_dir="/opt/node_n"
 node_version_path="$n_install_dir/n/versions/node"
 # N_PREFIX is the directory of n, it needs to be loaded as a environment variable.
